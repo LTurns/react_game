0aaed8ca566459682b71bdb0f98c84c5fd100cc3
Tom Stewart <TomStewart@operationss-air.home>
2020-08-13 00:50:42 +0100

    altering pics

--- src/components/play.js
+++ src/components/play.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import snooop from '../images/snooop.png'
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
 
 function Play(){
   return(
     <div className="play">
       <center>
+        <Row>
+        <Col xs={12} md={12}>
+        <Image src={snooop} id="snooop"/>
         <canvas id="gameScreen" width="1500" height="800"></canvas>
+        <Image src={snooop} id="snooop"/>
+        </Col>
+        </Row>
       </center>
     </div>
   )
